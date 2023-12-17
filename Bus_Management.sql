3165ab7ffd38fa9f5af21b481f5af7b9ad897997
The-mRu <122901187+The-mRu@users.noreply.github.com>
2023-12-17 18:05:01 +0600

    Update Bus_Management.sql

--- Bus_Management.sql
+++ Bus_Management.sql
@@ -1,46 +1,39 @@
 CREATE TABLE category(
-	category_id varchar(20) PRIMARY KEY,
-	category_name varchar(30) NOT NULL, 
+	category varchar(30) NOT NULL  PRIMARY KEY, 
 	seats  INTEGER NOT NULL,
-	oil_percentage  NUMERIC NOT NULL,
-	salary_percantage NUMERIC NOT NULL
-);
-
-
-
-CREATE TABLE driver(
-	driver_id INTEGER PRIMARY KEY,
-	name varchar(50),
-	age INTEGER NOT NULL,
- 	license_no  varchar(30)  NOT NULL
-
-);
-
-
-
-
-CREATE TABLE bus (
-    bus_id INTEGER PRIMARY KEY,
-    category_id varchar(20) NOT NULL,
-    license_no varchar(30) NOT NULL,
-    assign varchar(10) NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE route(
-	route_name varchar(30) PRIMARY KEY,
-	oil NUMERIC NOT NULL,
+	oil  NUMERIC NOT NULL,
 	salary NUMERIC NOT NULL
 );
 
 
 
+CREATE TABLE driver(
+	driver_id varchar(30) PRIMARY KEY,
+	name varchar(50),
+	age INTEGER NOT NULL,
+ 	license_no  varchar(30)  NOT NULL
+);
+
+
+
+CREATE TABLE bus (
+    bus_id varchar(20) PRIMARY KEY,
+    category varchar(30) NOT NULL REFERENCES category(category) ON DELETE CASCADE,
+    license_no varchar(30) NOT NULL,
+    assign varchar(30) NOT NULL
+);
+
+
+CREATE TABLE route(
+	route_name varchar(30) PRIMARY KEY,
+	distance NUMERIC NOT NULL
+);
+
 
 
 CREATE TABLE trip (
-  driver_id INT NOT NULL,
-  bus_id INT NOT NULL,
+  driver_id varchar(30) NOT NULL,
+  bus_id varchar(20) NOT NULL,
   date DATE NOT NULL,
   route_name VARCHAR(30) NOT NULL,
   FOREIGN KEY (driver_id) REFERENCES driver(driver_id) ON DELETE CASCADE,
@@ -50,8 +43,8 @@ CREATE TABLE trip (
 
 
 CREATE TABLE maintanance (
-  bus_id INT NOT NULL,
-  driver_id INT NOT NULL,
+  bus_id varchar(20) NOT NULL,
+  driver_id varchar(30) NOT NULL,
   issue VARCHAR(200) NOT NULL,
   estimated_cost NUMERIC NOT NULL,
   issue_date DATE NOT NULL,
@@ -59,6 +52,9 @@ CREATE TABLE maintanance (
   FOREIGN KEY (driver_id) REFERENCES driver(driver_id) ON DELETE CASCADE,
   FOREIGN KEY (bus_id) REFERENCES bus(bus_id) ON DELETE CASCADE
 );
+
+
+
 
 
 
@@ -83,42 +79,55 @@ VALUES
 
 
 INSERT INTO category(
-	category_id,category_name,seats,oil_percentage,salary_percantage
+	category,seats,oil,salary
 )
 VALUES
-	('01' ,'AC BUS', 40 , 1.5 , 2 ),
-	('02' ,'Mini AC BUS', 40 , 1.2 , 1.5 ),
-	('03' , 'Non AC', 50 , 1 , 1 );
+	('mini',30,0.15,1500),
+	('medium',50,0.2,1700),
+	('big',65,0.25,2000),
+	('double_decker',120,0.35,2500);
+---select *from category
 
 
 INSERT INTO bus(
-	bus_id,category_id,license_no,assign
-)
-VALUES
-
-	('01' ,'01', 'CTG-LA 11-1234' ,'idk' ),
-	('02' ,'01', 'DHA-LA 11-7923' ,'idk' ),
-	('03' ,'02', 'CTG-LA 11-1235' ,'idk' ),
-	('04' ,'03', 'CTG-LA 11-1236' ,'idk' );
+	bus_id,category,license_no,assign)
+	VALUES
+	('01' ,'mini', 'CTG-LA 11-1234' ,'student' ),
+	('02' ,'medium', 'DHA-LA 11-7923' ,'teacher' ),
+	('03' ,'big', 'CTG-LA 11-1235' ,'staff' ),
+	('04' ,'double_decker', 'CTG-LA 11-1236' ,'student' ),
+	('05' ,'mini', 'CTG-LA 11-1244' ,'student' ),
+	('06' ,'medium', 'DHA-LA 11-7943' ,'staff' ),
+	('07' ,'big', 'CTG-LA 11-1255' ,'teacher' ),
+	('08' ,'double_decker', 'CTG-LA 11-1266' ,'student' );
+---select *from bus	
 	
 
-INSERT INTO route(
-	route_name,oil,salary
+IINSERT INTO route(
+	route_name,distance
 )
 VALUES
+	('agrabad',28),
+	('bot',24),
+	('gec',26),
+	('chawkbazar',25),
+	('kotuwali',30),
+	('hathhazari',25);
+---select * from route
 
-	('Agrabad to IIUC', 5 , 3000 ),
-	('GSC to IIUC', 5 , 3000 ),
-	('BoT to IIUC', 5 , 3500 );
+
 
 INSERT INTO trip(
 	driver_id,bus_id,date,route_name
 )
 VALUES
-
-	( 1, 1,'2023-10-03','BoT to IIUC'),
-	( 2, 2,'2023-10-03','GSC to IIUC'),
-	( 3, 4,'2023-10-03','Agrabad to IIUC');
+	( '1', '01','2023-10-03','bot'),
+	( '2', '02','2023-10-03','gec'),
+	( '3', '03','2023-10-03','chawkbazar'),
+	( '4', '04','2023-10-03','agrabad'),
+	( '5', '05','2023-10-03','kotuwali'),
+	( '6', '06','2023-10-03','agrabad'),
+ 	( '7', '07','2023-10-03','bot');
 	
-
 	
+---select * from trip 
